888cb3e5f8c52329741c50d85788755b4bea1293
Frasier <fsundra@gmail.com>
2025-02-23 02:33:48 +0800

    comments added in nvim setup

--- nvim/lua/FConfig/settings.lua
+++ nvim/lua/FConfig/settings.lua
@@ -3,9 +3,15 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.smartindent = true
-vim.opt.guicursor = 'n-v-c:block,i-ci-ve:hor40,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff100-blinkon1500-Cursor'
-vim.opt.mouse= 'a'
 
+-- for my custom cursor
+-- unsure if this is actually works on linux
+-- windows doesnt like changing the width of certain cursor types
+-- rember: you can look at the documentation (:help guicursor)
+
+vim.opt.guicursor = 'n-v-c:block,i-ci-ve:hor40,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff100-blinkon1500-Cursor' 
+
+vim.opt.mouse= 'a'
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
