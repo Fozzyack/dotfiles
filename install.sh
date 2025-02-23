27ad57cd7b38421e015da08d8b63fa9da1f3654f
Fozzyack <fsundra@gmail.com>
2025-02-23 15:45:27 +0800

    Updates readme

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 
-echo "adding links"
+echo "UPDATING SYSTEM and making sure build-essentials are installed"
+sudo apt update && sudo apt upgrade --yes
+sudo apt install build-essential cmake
 
+echo "INSTALLING NODE VERSION MANAGER"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+mkdir ~/.config
+
+echo "ADDING SYMBOLIC LINKS"
 ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/alacritty ~/.config/alacritty
 
-echo "Setup Complete"
+echo "FINISHED"
