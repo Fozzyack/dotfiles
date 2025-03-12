2d6bf3060df60e5138ddf84ffbcc314bc4325189
Frasier <fsundra@gmail.com>
2025-03-13 03:57:17 +0800

    Adds changes to the tmux dotfile. - Makes windows start from 1 instead of 0.

--- nvim/lua/FConfig/maps.lua
+++ nvim/lua/FConfig/maps.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
 vim.keymap.set('v', '<leader>y', '"+y') 
 
 
