fda8b9337a62b1a3e708cb7aa247bead63f399c1
Frasier <fsundra@gmail.com>
2025-04-08 14:58:34 +0800

    CHANGES - Fixes Hyprpaper bug - Hyprland config change was just because I wanted to use a university monitor. - Adds fuzzy finder - Adds oh my posh TODO - Add install scripts for Fuzzy Finder for bash and fish - Update Readme - Update Installer

---
+++ ff/ff.fish
@@ -0,0 +1,21 @@
+# FISH
+# This is the shell script that is run when installed using dotfiles
+set home $HOME
+set output ("$home/dotfiles/ff/ffcli" $argv)
+set program_exit_status $status
+if test $program_exit_status -eq 5
+    echo "USAGE:"
+    echo ""
+    echo "ff [ DIRECTORY | [-s | --start ] | [ -h | --help ] ]"
+    echo ""
+    echo "ARGUMENTS:"
+    echo ""
+    echo "      DIRECTORY       Starting Directory"
+    echo "      -s, --start     Start searching from users home directory"
+    echo "      -h, --help      Print this help menu"
+else if test -d $output
+    cd "$output"
+else
+    echo "$output"
+end
+
