e3b9fc3f5d50425f374e743b58d76b4420550c8d
Frasier <fsundra@gmail.com>
2025-09-01 16:00:30 +0800

    formatting on <leader>w

--- configs/nvim/after/plugin/conform.lua
+++ configs/nvim/after/plugin/conform.lua
@@ -1,17 +1,24 @@
-require("conform").setup({
+local conform = require("conform")
+conform.setup({
     formatters_by_ft = {
         javascript = { "prettierd", "prettier", stop_after_first = true },
         go = { "gofmt" },
     },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback"
-    }
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-    end,
-})
+-- Setting to enable formatting on save
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--     pattern = "*",
+--     callback = function(args)
+--         require("conform").format({ bufnr = args.buf })
+--     end,
+-- })
+
+vim.keymap.set("n", "<leader>w", function()
+    print("Formatting")
+    conform.format({
+        lsp_format = "fallback",
+        async = true,
+        timeout_ms = 500
+    })
+end)
