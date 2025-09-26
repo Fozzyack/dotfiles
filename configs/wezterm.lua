4fff34d61e88504dc44bc8489aec8674629b529f
Frasier <fsundra@gmail.com>
2025-09-26 15:51:30 +0800

    Adding wezterm config

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -1,17 +1,91 @@
 local wezterm = require "wezterm"
 local config = {}
 
-config.color_scheme = "FrontEndDelight"
+config.color_scheme = "Snazzy"
 config.window_frame = {
 
-    font = wezterm.font("Hurmit Nerd Font", { weight = "Bold", italic = true })
+    font = wezterm.font("Hurmit Nerd Font", { weight = "Bold", italic = true }),
+    font_size = 14.0
 }
 config.default_prog = { "wsl.exe" }
 config.inactive_pane_hsb = {
     saturation = 0.9,
     brightness = 0.8,
 }
-config.window_background_image = ""
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#0b0022',
+
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = '#2b2042',
+      -- The color of the text for the tab
+      fg_color = '#c0c0c0',
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
+
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
+
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
+
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = false,
+    },
+
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over inactive tabs
+    inactive_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab_hover`.
+    },
+
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over the new tab button
+    new_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab_hover`.
+    },
+  },
+}
+config.window_background_image = "mnt/c/Users/fsund/Downloads/testpic.jpg"
 config.window_background_image_hsb = {
     -- Darken the background image by reducing it to 1/3rd
     brightness = 0.3,
@@ -23,5 +97,5 @@ config.window_background_image_hsb = {
     -- You can adjust the saturation also.
     saturation = 1.0,
 }
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 return config
