72251181d9524389358608d998d5d1539437982f
Frasier <fsundra@gmail.com>
2025-10-07 22:55:22 +0800

    update

--- configs/nvim/after/plugin/blink-completions.lua
+++ configs/nvim/after/plugin/blink-completions.lua
@@ -15,6 +15,11 @@ blink.setup({
         nerd_font_variant = 'mono'
     },
 
+    completion = {
+        accept = {
+            auto_brackets = { enabled = false }
+        }
+    },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
