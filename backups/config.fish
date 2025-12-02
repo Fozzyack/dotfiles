da98c746986dceade4c9c740895efdda8e7fa65f
Frasier <fsundra@gmail.com>
2025-12-03 01:22:29 +0800

    Adds: Fish config (fzf using plugin integration)

--- backups/config.fish
+++ backups/config.fish
@@ -1,23 +1,31 @@
 if status is-interactive # Commands to run in interactive sessions can go here
 end
 
-function ff; source ~/dotfiles/ff/ff.fish $argv; end
-function dfile; ~/dotfiles/dfile/dfile $argv; end
-alias ls exa
-starship init fish | source
-
+# Adding to the path
 export PATH="$PATH:/home/fozzyack/.dotnet/tools"
 export PATH="$PATH:$(go env GOPATH)/bin"
 export PATH="$PATH:/home/fozzyack/.runnables/zen"
 export PATH="$PATH:/home/fozzyack/.config/emacs/bin"
 
-# Set up fzf key bindings
-fzf --fish | source
+# Fastfetch (displays system info)
+fastfetch
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
-fastfetch
+# fzf setup -- fzf.fish (plugin required)
+fzf --fish | source
+fzf_configure_bindings --history=\cr --directory=\cf --variables=\cv --git_log=\ch --git_status=\cg
 
+
+#  some extra things
+alias ls exa
+function ff; source ~/dotfiles/ff/ff.fish $argv; end
+function dfile; ~/dotfiles/dfile/dfile $argv; end
+
+# Prompt manager
+starship init fish | source
+
+# For GPG keys to work (git stuff)
 set -x GPG_TTY (tty)
