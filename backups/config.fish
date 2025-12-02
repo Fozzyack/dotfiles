68b4a1e3f405c4c9103b93aa0497244a5fdcd147
Frasier <fsundra@gmail.com>
2025-12-03 01:50:11 +0800

    Adds extra aliases

--- backups/config.fish
+++ backups/config.fish
@@ -19,20 +19,39 @@ fzf --fish | source
 fzf_configure_bindings --history=\cr --directory=\cf --variables=\cv --git_log=\ch --git_status=\cg
 
 #  extra things
-alias ls exa # Uses exa instead of ls
 function ff; source ~/dotfiles/ff/ff.fish $argv; end
 function dfile; ~/dotfiles/dfile/dfile $argv; end
 
 # Prompt manager
 starship init fish | source
 
+# For GPG keys to work (git stuff)
+set -x GPG_TTY (tty)
+
+# ALIASES ---------------------
+
+# list
+alias ls exa # Uses exa instead of ls
+alias ll="exa -al"
+
+# grep
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+
+# Navigation
+alias ..="cd .."
+alias ...="cd ../../../"
+alias ....="cd ../../../../"
+
 # github
 alias gp="git push"
 alias gpl="git pull"
-alias ga="git add ."
+alias ga="git add"
+alias gaa="git add ."
 alias gc="git commit"
 alias gs="git status"
 alias gl="git log"
+alias gd="git diff"
 
-# For GPG keys to work (git stuff)
-set -x GPG_TTY (tty)
+# ALIASES END ----------------
