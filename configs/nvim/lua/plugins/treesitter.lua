0657d1366030819221c533669108fdce64f4c9bb
Frasier <fsundra@gmail.com>
2026-07-11 13:43:19 +0800

    updating lazy nvim config (treesitter)

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -4,5 +4,12 @@ return {
       branch = "main",
       lazy = false,
       build = ":TSUpdate",
+      config = function()
+          require('nvim-treesitter').setup {
+              -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+              install_dir = vim.fn.stdpath('data') .. '/site'
+          }
+          require('nvim-treesitter').install { 'go', 'javascript', 'zig', 'tsx', 'typescript', 'css', 'html', 'lua', 'python'}
+      end
     }
 }
