6eef6f21cb818d21f2e0f2d32dfa1353dd13080b
Frasier <fsundra@gmail.com>
2026-07-13 19:48:59 +0800

    changes treesitter setup

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,45 @@
 return {
     {
-      "nvim-treesitter/nvim-treesitter",
-      branch = "main",
-      lazy = false,
-      build = ":TSUpdate",
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            local nts = require("nvim-treesitter")
+            nts.setup({
+                install_dir = vim.fn.stdpath("data") .. "/site",
+            })
+
+            nts.install({
+                "javascript",
+                "typescript",
+                "tsx",
+                "html",
+                "css",
+                "json",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "go",
+                "python",
+                "bash",
+                "c",
+                "cpp"
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local lang = vim.treesitter.language.get_lang(args.match)
+
+                    if lang and vim.treesitter.language.add(lang) then
+                        vim.treesitter.start(args.buf, lang)
+                    end
+                end,
+            })
+        end,
+
+
+
     }
 }
