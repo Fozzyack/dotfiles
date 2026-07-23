baf532368974cb557733d7d0ace5c6332ac64aec
Frasier <fsundra@gmail.com>
2026-07-23 16:46:44 +0800

    adds a way to turn on and off neocodium completions

--- configs/nvim/lua/plugins/neocodium.lua
+++ configs/nvim/lua/plugins/neocodium.lua
@@ -1,11 +1,14 @@
 return {
-  "monkoose/neocodeium",
-  event = "VeryLazy",
-  config = function()
-    local neocodeium = require("neocodeium")
-    neocodeium.setup()
-    vim.keymap.set("i", "<C-l>", neocodeium.accept)
-    vim.keymap.set("i", "<C-h>", neocodeium.accept_word)
-  end,
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function()
+        local neocodeium = require("neocodeium")
+        neocodeium.setup()
+        vim.keymap.set("i", "<C-l>", neocodeium.accept)
+        vim.keymap.set("i", "<C-h>", neocodeium.accept_word)
+        vim.keymap.set("n", "<leader>z", function()
+            vim.cmd("NeoCodeium toggle")
+        end)
+        vim.cmd([[NeoCodeium disable]])
+    end,
 }
-
