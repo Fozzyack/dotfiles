e0f811f4f0c27d7dc8b968e9568f79be1824be05
Frasier <fsundra@gmail.com>
2026-08-25 15:30:10 +0800

    updates harpoon

--- configs/nvim/after/plugin/harpoon.lua
+++ configs/nvim/after/plugin/harpoon.lua
@@ -1,23 +1,25 @@
-vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
-vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
-vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
-vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
-vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
+-- OLD CONFIG
+-- vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
+-- vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+-- vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+-- vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
+-- vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
 
 
-local harpoonMark = require("harpoon.mark")
-local harpoonUI = require("harpoon.ui")
+local harpoon = require("harpoon");
+
+harpoon:setup()
 
 vim.keymap.set("n", "<leader>e", function()
-    harpoonUI.toggle_quick_menu()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
 vim.keymap.set("n", "<leader>a", function()
-    harpoonMark.add_file()
+    harpoon:list():add()
 end)
 
 for i = 0, 9 do
     vim.keymap.set("n", "<leader>" .. i, function()
-        harpoonUI.nav_file(i)
+        harpoon:list():select(i)
     end)
 end
