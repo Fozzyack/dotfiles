1e326f487b80eca2d78d32daae591a43402ef8c7
Frasier <fsundra@gmail.com>
2026-08-25 18:07:41 +0800

    Adds hidden files to telescope search

--- configs/nvim/after/plugin/telescope.lua
+++ configs/nvim/after/plugin/telescope.lua
@@ -1,3 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>f', ":Telescope find_files hidden=true<CR>", { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
