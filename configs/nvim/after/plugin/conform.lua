77ec98d0baa954399aa59cd02d9766c49d86a8a6
Frasier <fsundra@gmail.com>
2026-09-07 22:28:38 +0800

    removes neogit -- using lazy git

--- configs/nvim/after/plugin/conform.lua
+++ configs/nvim/after/plugin/conform.lua
@@ -6,7 +6,7 @@ conform.setup({
         typescript = { "prettier", "prettierd", stop_after_first = true },
         typescriptreact = { "prettier", "prettierd", stop_after_first = true },
         cs = { "csharpier" },
-        python = { "autopep8", stop_after_first = true },
+        python = { "autopep8", "black", stop_after_first = true, lsp_format = "fallback" },
         c = { "clang-format" },
     }
 })
--- configs/nvim/lua/plugins/git.lua
+++ configs/nvim/lua/plugins/git.lua
@@ -1,19 +1,19 @@
 return {
     {
+        "lewis6991/gitsigns.nvim"
+    },
+    {
         "NeogitOrg/neogit",
         lazy = true,
         dependencies = {
-            "esmuellert/codediff.nvim",  -- optional
-            "nvim-telescope/telescope.nvim", -- optional
+            "esmuellert/codediff.nvim", -- optional
+
+            -- For a custom log pager
+            "m00qek/baleia.nvim", -- optional
         },
         cmd = "Neogit",
         keys = {
             { "<leader><leader>g", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
-
-
         }
-    },
-    {
-        "lewis6991/gitsigns.nvim"
-    },
+    }
 }
